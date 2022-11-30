6fb8d17da645a4530b19f1bee78429e3c84fc5c9
21f1006304 <119501805+21f1006304@users.noreply.github.com>
2022-11-30 20:20:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import streamlit as st
-#import pandas as pd
-#from sklearn import datasets
-#from sklearn.ensemble import RandomForestClassifier
-#import pickle
 st.write("""
-# credit card default prediction
-this app do something
+# App to do subtraction
+This will display the absolute value of subtraction of two numbers
 """)
 st.header('user input parameters')
 def user_input_features():
-  number1 = st.number_input("NUMBER1",min_value=0.,max_value=100.0)
-  number2 = st.number_input("NUMBER2",min_value=0.,max_value=100.0)
+  number1 = st.number_input("NUMBER1",min_value=0.)
+  number2 = st.number_input("NUMBER2",min_value=0.)
   data = {'NUMBER1': number1,
       'NUMBER2': number2
   }
@@ -22,5 +18,5 @@ def user_input_features():
     return 0
 
 df = user_input_features()
-st.subheader("This is subheader")
+st.subheader("Result:")
 st.write(df)
