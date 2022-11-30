785a8ee0d2c17acbc22bc9d803a918122aaef57e
21f1006304 <119501805+21f1006304@users.noreply.github.com>
2022-11-30 20:21:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,10 @@ st.header('user input parameters')
 def user_input_features():
   number1 = st.number_input("NUMBER1",min_value=0.)
   number2 = st.number_input("NUMBER2",min_value=0.)
-  data = {'NUMBER1': number1,
-      'NUMBER2': number2
-  }
   if (number1 > number2) :
     return number1 - number2
   elif (number2 > number1) :
-    number2 - number1
+    return number2 - number1
   else :
     return 0
 
